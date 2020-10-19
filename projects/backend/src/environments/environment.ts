6aafaaae4120c516ab044b7a17bd9cbc5066dfd4
Ricardo Watanabe Bento <roicoroy@yahoo.com.br>
2020-10-19 19:00:41 +0100

    first

--- projects/backend/src/environments/environment.ts
+++ projects/backend/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyABAfAdtrtuVW8Pmcn3Dzj5MaD7OxKmOwY",
+    authDomain: "ngx-firebase-cms-2301.firebaseapp.com",
+    databaseURL: "https://ngx-firebase-cms-2301.firebaseio.com",
+    projectId: "ngx-firebase-cms-2301",
+    storageBucket: "ngx-firebase-cms-2301.appspot.com",
+    messagingSenderId: "474133862114",
+    appId: "1:474133862114:web:4964a0b2ecbc1ff674b0bb",
+    measurementId: "G-0MXQRF40YF"
+  }
 };
 
 /*
